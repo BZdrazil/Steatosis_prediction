92a426ea3eb03a843154e728ed830949e009c2da
BZdrazil <51444479+BZdrazil@users.noreply.github.com>
2020-10-29 17:29:13 +0100

    Update conformal_prediction.py

--- Code_Conformal_Prediction/conformal_prediction.py
+++ Code_Conformal_Prediction/conformal_prediction.py
@@ -22,22 +22,24 @@ from sklearn.utils import shuffle
 import cloudpickle
 
 parser = ArgumentParser()
-parser.add_argument('-i','--infile', help='input training file (tab sep)')
+parser.add_argument('-i','--infile', help='input training file')
 parser.add_argument('-n','--nmodels', type=str, help='number of models (default 20 models)')
 parser.add_argument('-m','--mode', type=str, choices=['t','p', 'b'], help='mode: build models, predict new data from models, both build and predict')
-parser.add_argument('-p','--predfile', help='input prediction file (tab sep) if mode == p')
+parser.add_argument('-s','--sep', type=str, choices=['t','c'], help='file separator: tab or comma')
+parser.add_argument('-p','--predfile', help='input prediction file if mode == p')
 args = parser.parse_args()
 
 infile = args.infile
 nmodels = args.nmodels
 mode = args.mode
+sep = args.sep
 predfile = args.predfile
 
-if infile == None or mode == None:
+if infile == None or mode == None or sep == None:
     parser.print_help(sys.stderr)
     sys.exit(1)
 
-if mode == 'p' and predfile == None:
+if mode == 'p' and predfile == None or mode == 'b' and predfile == None:
     parser.print_help(sys.stderr)
     sys.exit(1)
 
@@ -51,18 +53,26 @@ if mode != 'p':
         os.remove(modelfile)
     f= open(modelfile, mode='ab')
     cloudpickle.dump(nmodels, f)
-    data = pd.read_csv(infile, sep='\t', header = 0, index_col = None)
+    if sep == 't':
+        data = pd.read_csv(infile, sep='\t', header = 0, index_col = None)
+    if sep == 'c':
+        data = pd.read_csv(infile, sep=',', header = 0, index_col = None)
+    if 'name' or 'Name' or 'Molecule name' in data.columns:
+        data.rename(columns={'name': 'id', 'Name': 'id', 'Molecule name': 'id'}, inplace=True)
+    if 'class' in data.columns:
+        data.rename(columns={'class': 'target'}, inplace=True)
     data.loc[data['target'] < 0, 'target'] = 0
     target = data['target'].values
-    train = data.drop(['id'], axis=1)
-    train = train.drop(['target'], axis=1).values
+    train = data.drop(['id'], axis=1, errors='ignore')
+    train = train.drop(['dataset'], axis=1, errors='ignore')
+    train = train.drop(['target'], axis=1, errors='ignore').values
     part1 = int(0.7*len(train))
     for xx in range(1, nmodels+1):
         modelfile2 = infile +"_nonconf"+"_"+str(xx)+".model"
         print ("Working on model", xx)
  
         idx = np.random.permutation(int(len(train)))
-        print (idx)
+#        print (idx)
         trainset = idx[:part1]
         calset = idx[part1:]
 
@@ -75,22 +85,29 @@ if mode != 'p':
     f.close()
 
 if mode != 't':
-    outfile = predfile + "_nonconf_pred100sum.csv"
-    f2 = open(outfile,'w')
-    f2.write('id\tp-value_low_class\tp-value_high_class\tclass\tmodel\n')
-    f2.close()
-
-    data = pd.read_csv(predfile, sep='\t', header = 0, index_col = None)
+    if sep == 't':
+        data = pd.read_csv(predfile, sep='\t', header = 0, index_col = None)
+    if sep == 'c':
+        data = pd.read_csv(predfile, sep=',', header = 0, index_col = None)
+    if 'name' or 'Name' or 'Molecule name' in data.columns:
+        data.rename(columns={'name': 'id', 'Name': 'id', 'Molecule name': 'id'}, inplace=True)
+    if 'class' in data.columns:
+        data.rename(columns={'class': 'target'}, inplace=True)
     data.loc[data['target'] < 0, 'target'] = 0
     labels = data['id']
     ll = len(labels)
     target = data['target'].values
-    test = data.drop(['id'], axis=1)
-    test = test.drop(['target'], axis=1).values
+    test = data.drop(['id'], axis=1, errors='ignore')
+    test = test.drop(['dataset'], axis=1, errors='ignore')
+    test = test.drop(['target'], axis=1, errors='ignore').values
 
     f= open(modelfile, mode='rb')
     nmodels_built = cloudpickle.load(f)
     print ("Models built:",  nmodels_built)
+    outfile = predfile + "_nonconf_pred" + str(nmodels_built) + "sum.csv"
+    f2 = open(outfile,'w')
+    f2.write('id\tp-value_low_class\tp-value_high_class\tclass\tmodel\n')
+    f2.close()
     if nmodels > nmodels_built:
         print ("More models ordered (",nmodels,") than the file contains. Setting the number of models to built models.")
         nmodels = nmodels_built
@@ -107,4 +124,3 @@ if mode != 't':
     f.close()
 
 print (" - finished\n")
- 
